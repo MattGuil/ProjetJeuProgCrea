db299ed45fb26d8abaa2dbfe8b75d8292eae6449
MattGuil <guillemin.matt@gmail.com>
2024-11-19 17:46:15 +0100

    make human opacity dynamic

--- Human.js
+++ Human.js
@@ -21,17 +21,22 @@ class Human {
     this.frame = pFrame;
     this.hWidth = 5;
     this.hHeight = 5;
+    this.opacity = 0;
   }
 
   // Dessine le pied ou la main à l'écran
   draw() {
     translate(-this.hWidth / 2, -this.hHeight / 2);
+    push();
+    tint(255, 255, 255, this.opacity);
     image(this.frame, this.x, this.y, this.hWidth, this.hHeight);
+    pop();
     translate(this.hWidth / 2, this.hHeight / 2);
 
     // On incrémente la taille du pied ou de la main pour simuler une attaque en approche
     this.hWidth++;
     this.hHeight++;
+    this.opacity++;
   }
 
   // Détecte une éventuelle collision avec les coordonnées de Marie la fourmi qui sont passées en paramètres
